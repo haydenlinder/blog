676bbec4c6958accf16e0f1e659026113f4db834
haydenlinder <haydenlinder@gmail.com>
2021-05-10 08:24:16 -0700

    Add dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import '../styles/globals.css'
-import 'highlight.js/styles/vs.css';
-import 'highlight.js/styles/vs2015.css';
 import Container from '@material-ui/core/Container'
 import MenuItems from '../components/MenuItems';
 import manifest from '../scripts/manifest';
@@ -8,35 +6,37 @@ import Box from '@material-ui/core/Box';
 import Switch from '@material-ui/core/Switch';
 import Brightness7Icon from '@material-ui/icons/Brightness7';
 import Brightness4Icon from '@material-ui/icons/Brightness4';import { useState } from 'react';
+import '../styles/vs2015.css';
+import '../styles/vs.css';
 
 function MyApp({ Component, pageProps }) {
 
-  const [isDark, setIsDark] = useState(false)
+  const [isDark, setIsDark] = useState(true)
 
   const toggleDarkMode = e => {
     setIsDark(e.target.checked)
   }
 
-  console.log({isDark})
-
   return (
-    <Container>
-      <Box display='flex'>
-        <MenuItems titles={manifest} />
-        <Switch
-          checked={isDark}
-          color='primary'
-          onChange={toggleDarkMode}
-          name="dark-mode"
-          icon={<Brightness4Icon/>}
-          checkedIcon={<Brightness7Icon />}
-          inputProps={{ 'aria-label': 'secondary checkbox' }}
-        />
-        <Container maxWidth='md'>
-          <Component {...pageProps} />
-        </Container>
-      </Box>
-    </Container>
+    <div className={isDark?'dark':'light'} >
+      <Container>
+        <Box display='flex'>
+          <MenuItems titles={manifest} />
+          <Switch
+            checked={isDark}
+            color='primary'
+            onChange={toggleDarkMode}
+            name="dark-mode"
+            icon={<Brightness4Icon/>}
+            checkedIcon={<Brightness7Icon />}
+            inputProps={{ 'aria-label': 'secondary checkbox' }}
+          />
+          <Container maxWidth='md'>
+            <Component {...pageProps} />
+          </Container>
+        </Box>
+      </Container>
+    </div>
   )
 }
 
